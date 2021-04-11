493c09099245f2fdd45b4d3933e26b0e09b0e37b
Sakshi Kolekar <sakshikolekar03@gmail.com>
2021-04-11 15:10:25 +0530

    patterns pattern8

--- Patterns/Pattern8.java
+++ Patterns/Pattern8.java
@@ -2,27 +2,26 @@
 2. You've to create a pattern of * and separated by tab as shown in output format.
 eg, n=5
 o/p=
-*	
-	*	
-		*	
-			*	
 				*	
+			*		
+		*			
+	*				
+*
 */
-//Note this can be done using line equation too
+
 import java.util.*;
 
-public class Pattern7 {
+public class Pattern8 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
         // write ur code here
-        int n=scn.nextInt();
-        
-        for(int i=0; i<n;i++)
-        {
-            for(int j=0;j<i;j++)
-            System.out.print("	");
+        int n = scn.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = n-1; j > i; j--)
+                System.out.print("	");
             System.out.print("*");
             System.out.println();
         }
