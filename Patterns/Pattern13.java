ca3775122a5c239fa578e14a8d5611d2c4aa5689
Sakshi Kolekar <sakshikolekar03@gmail.com>
2021-04-14 23:43:56 +0530

    patterns pattern13

--- Patterns/Pattern13.java
+++ Patterns/Pattern13.java
@@ -1,6 +1,3 @@
-public class Pattern13 {
-    
-}
 /*1. You are given a number n.
 2. You've to create a pattern as shown in sample output.
 eg, n=5
@@ -11,3 +8,27 @@ Sample Output
 1	3	3	1	
 1	4	6	4	1
 */
+import java.util.*;
+public class Pattern13 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        //write your code here
+        int n=scn.nextInt();
+        
+        
+        int ncr=1;
+        for(int i=0;i<n;i++)
+        {
+            ncr=1;
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print(ncr+"	");
+                ncr=ncr *(i-j)/(j+1);
+            }
+            
+                System.out.println();
+        }
+
+    }
+}
