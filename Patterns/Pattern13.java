d50a58ae235d2b58a8e801e89946c598e213c2c9
Sakshi Kolekar <sakshikolekar03@gmail.com>
2021-04-14 23:42:36 +0530

    patterns pattern13

---
+++ Patterns/Pattern13.java
@@ -0,0 +1,13 @@
+public class Pattern13 {
+    
+}
+/*1. You are given a number n.
+2. You've to create a pattern as shown in sample output.
+eg, n=5
+Sample Output
+1	
+1	1	
+1	2	1	
+1	3	3	1	
+1	4	6	4	1
+*/
